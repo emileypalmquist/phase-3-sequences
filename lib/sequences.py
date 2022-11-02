294b035bed7c95ada993be70424a6c015a3102ba
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-02 14:00:28 -0700

    tests passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = list()
+
+    for i in range(length):
+        if len(fib_list) > 1:
+            new_num = fib_list[i - 2] + fib_list[i - 1]
+            fib_list.append(new_num)
+        else:
+            fib_list.append(i)
+    print(fib_list)
